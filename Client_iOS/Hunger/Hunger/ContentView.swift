61503e5920cdb4f98aa83ec3ad1cfad2360013f2
Roman Reimche <roman@romanreimche.com>
2019-09-23 17:37:20 +0200

    Init

---
+++ Client_iOS/Hunger/Hunger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hunger
+//
+//  Created by Roman Reimche on 23.09.19.
+//  Copyright © 2019 Roman Reimche. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Client_iOS/Hunger/HungerTests/HungerTests.swift
@@ -0,0 +1,34 @@
+//
+//  HungerTests.swift
+//  HungerTests
+//
+//  Created by Roman Reimche on 23.09.19.
+//  Copyright © 2019 Roman Reimche. All rights reserved.
+//
+
+import XCTest
+@testable import Hunger
+
+class HungerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
